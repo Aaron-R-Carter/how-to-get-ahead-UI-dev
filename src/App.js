8efbe44df5291b30947ec537f30f2409067b57d8
Aaron-R-Carter <aaronrobertcarter@gmail.com>
2020-02-23 16:52:50 -0500

    first commit, includes create-react-app, FS cleanup, and stepper component

---
+++ src/App.js
@@ -0,0 +1,18 @@
+import React from 'react';
+import './App.css';
+import Stepper from "./Components/Stepper";
+
+function App() {
+  return (
+    <div>
+
+      <Stepper
+      >
+
+      </Stepper>
+      
+    </div>
+  );
+}
+
+export default App;
