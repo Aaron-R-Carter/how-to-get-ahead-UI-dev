085a19e90e2ef24a53d876297bfc1feb08012557
Aaron-R-Carter <aaronrobertcarter@gmail.com>
2020-02-23 20:49:07 -0500

    MyCard added into stepper part 1

--- src/App.js
+++ src/App.js
@@ -1,16 +1,18 @@
 import React from 'react';
 import './App.css';
 import Stepper from "./Components/Stepper";
+import Navbar from "./Components/Navbar";
+import MyContainer from "./Components/MyContainer";
+
 
 function App() {
   return (
     <div>
+     
+        <Navbar />
 
-      <Stepper
-      >
+        <Stepper />
 
-      </Stepper>
-      
     </div>
   );
 }
